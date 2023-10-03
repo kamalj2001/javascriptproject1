04e228eb14e0484b3b0af792e269451318780cf7
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-03 19:57:27 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,7 @@
+const kamal1 = "kamal"; //string datatype
+const kamal2 = 123; //number data tyl]pe
+const boolean = true; //boolean
+var aman; //undefined
+var nulls = null; //null
+
+console.table([kamal1,kamal2,boolean,aman,nulls]);
