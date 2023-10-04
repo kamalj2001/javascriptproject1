9016e928257ebad339329ea964b52e0fd5ac46b7
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-04 07:12:04 +0000

    strong type javascript datatypes

--- datatype.js
+++ datatype.js
@@ -4,4 +4,6 @@ const boolean = true; //boolean
 var aman; //undefined
 var nulls = null; //null
 
-console.table([kamal1,kamal2,boolean,aman,nulls]);
+const id = Symbol("jashan"); //symbol formet datatypes in javascript
+const data = BigInt(1234567890); //last chareter use n value infinative
+console.table([kamal1,kamal2,boolean,aman,nulls,id,data]);
