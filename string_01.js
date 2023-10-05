c9b43927618d24ce24fdce8c096ccf20a353e5bb
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-05 07:22:04 +0000

    kamal

---
+++ string_01.js
@@ -0,0 +1,14 @@
+//const name = "kamal";
+//const repoCount = 50;
+//console.table([`MY NAME IS ${name} Repocount ${repoCount}`]);
+
+const myTable = new String("lovely");
+
+console.log(myTable.length);
+console.log(myTable.toLocaleUpperCase());
+console.log(myTable.charAt(2));
+console.log(myTable.toLocaleUpperCase());
+console.log(myTable.indexOf('e'));
+console.log(myTable.toLocaleUpperCase());
+
+
