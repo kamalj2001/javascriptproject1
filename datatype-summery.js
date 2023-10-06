9207eeb8fd52806a369bf73815552aa2fb2155b0
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-06 04:39:47 +0000

    stack

---
+++ datatype-summery.js
@@ -0,0 +1,19 @@
+//stack or hip use in javascript
+
+//stack use premitive data type use 
+//hip use non permitive data type use
+
+let fristvalue = "kamaljeet";
+//console.log(fristvalue);
+let othervalue = fristvalue;
+othervalue = "jmd";
+console.log(othervalue);
+
+let userOne = {
+    email:"test.com",
+    upi : "upi@7421"
+}
+let userTwo = userOne;
+userTwo.email="jmd@gmail.com";
+console.log(userOne);
+console.log(userTwo); 
