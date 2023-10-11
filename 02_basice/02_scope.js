a7b02cd3516ae82a8e5d92144de1899fbed58e5f
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-11 08:51:59 +0000

    scope chan

---
+++ 02_basice/02_scope.js
@@ -0,0 +1,31 @@
+// let book = "sdfghjk";
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     console.log(a);
+// }
+// console.log(book);
+
+//nested scope
+
+// function one(){
+//     const username = "kamaljeet";
+//     function two(){
+//         const website = "youtube";
+//         console.log(username);
+//     }
+     //console.log(website);
+//     two();
+// }
+// one();
+
+
+if (true) {
+    const username02 = "pooja";
+    if (username02 === "pooja") {
+        const site = "webcubsoftware";
+        console.log(username02 + site);
+    }
+    //console.log(site);
+}
+//console.log(username02);
