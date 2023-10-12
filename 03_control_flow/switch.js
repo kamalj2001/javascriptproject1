0b0df3d046274ac09554657f5356f0677d12bd5e
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-12 09:44:25 +0000

    switch statement

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const dates = new Date();
+const todaydate = dates.getDate();
+console.log(todaydate);
+
+switch (todaydate) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    case 8:
+        console.log("Sunday");
+        break;
+    case 9:
+        console.log("Monday");
+        break;
+    case 10:
+        console.log("Tuesday");
+        break;
+    case 11:
+        console.log("Wednesday");
+        break;
+    case 12:
+        console.log("Thursday toady");
+        break;
+    case 13:
+        console.log("Friday");
+        break;
+    default:
+        console.log("default");
+        break;
+}
