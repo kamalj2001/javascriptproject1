8967d8b1b906ccbba68e1f4440cb9ff2a01f92c8
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-13 10:21:12 +0000

    loop while and do while

---
+++ 04_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0;
+// while (index <= 22) {
+//     console.log(`This number is ${index}`);
+//     index = index + 3;
+// }
+
+// const myArray = ["apple", "mango", "red", "elephant", "monkey"];
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+let score = 11;
+do {
+    console.log(`${score}`);
+    score++
+} while (score <= 10);
