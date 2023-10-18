2d17462c4112e85b96f0c1601b185703628233bd
kamalj2001 <135494917+kamalj2001@users.noreply.github.com>
2023-10-18 07:45:59 +0000

    foreach loop

--- 04_iterations/five.js
+++ 04_iterations/five.js
@@ -1,1 +1,43 @@
- 
+const codding = ["js", "ruby", "java", "pyton", "php"];
+// codding.forEach(function (val){
+//     console.log(val);
+// });
+
+codding.forEach( (val) =>{
+    //console.log(val);
+});
+
+function printMe(item) {
+    //console.log('====================================');
+    //console.log(item);
+    //console.log('====================================');
+}
+
+codding.forEach(printMe);
+
+codding.forEach((item, index, arr) =>{
+    //console.log(item, index, arr);
+});
+
+
+const myCodding = [
+    {
+        languagename : "hyper text markup lanuage",
+        shortname : 'html'
+    },
+    {
+        languagename : "case cadding style seet",
+        shortname : 'css'
+    },
+    {
+        languagename : "java script",
+        shortname : 'js'
+    }
+]
+
+myCodding.forEach((key , value) =>{
+        //console.log('====================================');
+        //console.log(key , value);
+        //console.log('====================================');
+        console.log(key.languagename);
+});
